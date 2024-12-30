65a01052f3af8b9c80371338cc583ab25fae6fcd
Zac Lambert <zlambert@solomonic.co.uk>
2024-12-30 12:21:53 +0000

    learning interfaces and table tests

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -14,52 +16,68 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-// func ExamplePerimeter() {
-// 	rectangle := Rectangle{10.0, 5.0}
-// 	perimeter := rectangle.Perimeter()
-// 	fmt.Printf("%.2f", perimeter)
-// 	// Output: 30.00
-// }
+func ExampleRectangle_Perimeter() {
+	rectangle := Rectangle{10.0, 5.0}
+	perimeter := rectangle.Perimeter()
+	fmt.Printf("%.2f", perimeter)
+	// Output: 30.00
+}
 
-// func BenchmarkPerimeter(b *testing.B) {
-// 	rectangle := Rectangle{10.0, 5.0}
-// 	for i := 0; i < b.N; i++ {
-// 		rectangle.Perimeter()
-// 	}
-// }
+func BenchmarkPerimeter(b *testing.B) {
+	rectangle := Rectangle{10.0, 5.0}
+	for i := 0; i < b.N; i++ {
+		rectangle.Perimeter()
+	}
+}
 
 // AREA
 
-func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 5.0}
-		got := rectangle.Area()
-		want := 50.00
-
-		if got != want {
-			t.Errorf("got %.2f, want %.2f", got, want)
-		}
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
-	})
+var areaTests = []struct {
+	shape Shape
+	want  float64
+}{
+	{shape: Rectangle{Width: 10.0, Height: 5.0}, want: 50.0},
+	{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
+	{shape: Triangle{Base: 5.0, Height: 6.0}, want: 15.0},
 }
 
-// func ExampleArea() {
-// 	rectangle := Rectangle{10.0, 5.0}
-// 	area := Area(rectangle)
-// 	fmt.Printf("%.2f", area)
-// 	// Output: 50.00
-// }
-//
-// func BenchmarkArea(b *testing.B) {
-// 	rectangle := Rectangle{10.0, 5.0}
-// 	for i := 0; i < b.N; i++ {
-// 		Area(rectangle)
-// 	}
-// }
+func TestArea(t *testing.T) {
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %.2f, want %.2f", tt.shape, got, tt.want)
+		}
+	}
+}
+
+func exampleArea(shape Shape) {
+	fmt.Printf("%.2f", shape.Area())
+}
+
+func ExampleCircle_Area() {
+	circle := Circle{10.0}
+	exampleArea(circle)
+	// Output: 314.16
+}
+
+func ExampleRectangle_Area() {
+	rectangle := Rectangle{10.0, 5.0}
+	exampleArea(rectangle)
+	// Output: 50.00
+}
+
+func ExampleTriangle_Area() {
+	triangle := Triangle{5.0, 6.0}
+	exampleArea(triangle)
+	// Output: 15.00
+}
+
+func BenchmarkShapes(b *testing.B) {
+	for _, tt := range areaTests {
+		b.Run(reflect.TypeOf(tt.shape).String(), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				tt.shape.Area()
+			}
+		})
+	}
+}

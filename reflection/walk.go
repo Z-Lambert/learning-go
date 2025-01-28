638474d2203eeb7d49cee32949551958daae6b83
Zac Lambert <77593462+Z-Lambert@users.noreply.github.com>
2025-01-28 10:24:01 +0000

    reflection with pointers

--- reflection/walk.go
+++ reflection/walk.go
@@ -3,11 +3,26 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	value := reflect.ValueOf(x)
-	for i := 0; i < value.NumField(); i++ {
-	field := value.Field(i)
-	if field.Kind() == reflect.String {
-		fn(field.String())
+	val := getValue(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+	
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
 	}
+
+	return val
 }

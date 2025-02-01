22f64639ec99b330ac5e07b91b2b0ebc7dec5315
Zac Lambert <77593462+Z-Lambert@users.noreply.github.com>
2025-02-01 11:12:28 +0000

    maps and channels

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -15,6 +15,19 @@ type Profile struct {
 	City string
 }
 
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %v to contain %q but it didn't", haystack, needle)
+	}
+}
+
 func TestWalk(t *testing.T) {
 
 	cases := []struct {
@@ -91,4 +104,39 @@ func TestWalk(t *testing.T) {
 			}
 		})
 	}
+	// New test for maps because maps do not guarantee order, so contains is used instead of reflect.DeepEqual
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Cow":   "Moo",
+			"Sheep": "Baa",
+		}
+	
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+	
+		assertContains(t, got, "Moo")
+		assertContains(t, got, "Baa")
+	})
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+	
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Katowice"}
+			close(aChannel)
+		}()
+	
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+	
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+	
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
